00dd3746382b3b57f5fa6dcc1140ef94b01181a4
EC2 Default User <ec2-user@ip-172-31-13-36.ap-northeast-1.compute.internal>
2022-11-17 04:34:30 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,35 @@
 Rails.application.routes.draw do
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   devise_for :users
+
+  namespace :admin do
+    root :to =>"homes#top"
+    resources :items, only: [:index, :new, :show, :edit, :create, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
+  scope module: :public do
+    root :to =>"homes#top"
+    get "homes/about"=>'homes#about', as: 'about'
+    get "customers/unsubscribed"=>'customers#check', as: 'customers/check'
+    get "customers/withdrawal"=>'customers#withdrawal', as: 'customers/withdrawal'
+    get "cart_items/destroy_all"=>'cart_items#destroy_all', as: 'cart_items/destroy_all'
+    get "orders/check"=>'orders#check', as: 'orders/check'
+    get "orders/complete"=>'orders#complete', as: 'orders/complete'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update]
+    resources :cart_items, only: [:index, :update,:destroy, :create]
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destory]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
