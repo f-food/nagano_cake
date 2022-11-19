0a3852b1f5b15685a11ed71ad85487f9f320fcfe
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-11-19 13:04:15 +0000

    Add

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,11 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
+    @genres = Genre.all
+    @items=Item.page(params[:page]).per(4)
   end
-  
+
   def about
   end
-  
+
 end
