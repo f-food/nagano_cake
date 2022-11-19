61fbe177dc57373ea97c9113a5154f828e9c1499
BRK-munger <katou.kai.309@gmail.com>
2022-11-19 04:30:19 +0000

    [Fix]　新規登録、一覧画面(管理者側)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,21 +1,22 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
 
   def new
     @item = Item.new
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
     @item.save
-    redirect_to admin_items_path
+    redirect_to admin_items_path(@item.id)
   end
 
   def index
-    @item = Item.new
-    @items = Item.all.page(params[:page])
+    @items = Item.page(params[:page])
   end
 
   def show
+    @items = Item.find(params[:id])
   end
 
   def edit
@@ -25,8 +26,10 @@ class Admin::ItemsController < ApplicationController
   end
 
 
+#投稿データのストロングパラメータ
   private
   def item_params
     params.require(:item).permit(:image, :name, :caption, :genre_id, :excluded_price, :is_status)
   end
+
 end
