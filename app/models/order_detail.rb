2d7332c53c5a2238ddc5db87415a9d13017af263
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-11-19 07:59:45 +0000

    Add

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,6 +2,6 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
   
-  enum make_status: { production_not_allowed: 0, waiting_for_production: 1, under_construction: 2, production_completed: 3}
+  enum making_status: { production_not_allowed: 0, waiting_for_production: 1, under_construction: 2, production_completed: 3}
   
 end
