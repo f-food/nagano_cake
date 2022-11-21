2bfb9d099578714f9743dcddaa6de5433d91faea
EC2 Default User <ec2-user@ip-172-31-22-83.ap-northeast-1.compute.internal>
2022-11-21 04:30:12 +0000

    [Fix]orderモデル内の記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,10 @@ class Order < ApplicationRecord
     customer.last_name + " " + customer.first_name
   end
 
+  def post_code_and_address
+    self.post_code + '(' + self.address.to_s + ')'
+  end
+
   enum payment_method: {credit_card: 0, transfer: 1 }
   enum status: {payment_wait: 0,payment_confirm: 1,making: 2,shipping_preparation: 3,shipped: 4}
 end
