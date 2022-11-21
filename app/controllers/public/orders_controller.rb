7f2957e3ccdb273bc2dbcf6ebfed889585abf6db
EC2 Default User <ec2-user@ip-172-31-22-83.ap-northeast-1.compute.internal>
2022-11-21 03:06:44 +0000

    [Add]注文情報,入力,確認,完了画面,機能

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,59 +4,60 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @customer = Customer.find(current_customer.id)
-    @address = current_customer.addresses.all
+    @addresses = current_customer.addresses.all
   end
 
   def create
     @order = current_customer.orders.new(order_params)
     @cart_items = current_customer.cart_items.all
-    @address = current_customer.addresses.new(address_params)
-    if @order.save
-      @cart_items.each do |cart_item|
-        @order_details = @order.order_details.new
-        @order_details.item_id = cart_item.item_id
-        @order_details.quantity = cart_item.quantity
-        @order_details.inclusive_price = (cart_item.item.excluded_price * 1.08).floor
-        @order_details.save
-      end
-      if params[:order][:address_number] == "3" #新しいお届け先の場合
-        @address.save
+    @shipping_address = current_customer.addresses.new(address_params)
+    @order.save
+    #　注文詳細実装後
+      #@cart_items.each do |cart_item|
+        #@order_detail = @order.order_detail.new
+        #@order_detail.item_id = cart_item.item_id
+        #@order_detail.quantity = cart_item.quantity
+        #@order_detail.inclusive_price = (cart_item.item.excluded_price * 1.08).floor
+        #@order_detail.save
+
+      if params[:order][:address_number] == "3"
+        @shipping_address.save
       end
       @cart_items.destroy_all
-      redirect_to items_path #後で購入完了画面に変更
-    else
-      render :new
-    end
+      redirect_to complete_orders_path
+      #else
+      #render :new
+    #end
   end
 
   def check
     @order = Order.new(order_params)
-    if params[:order][:address_number] == "1" #会員情報に登録している住所の場合
-      @order.name = current_customer.full_name
-      @order.address = current_customer.address
+    if params[:order][:address_number] == "1"
       @order.post_code = current_customer.post_code
-    elsif params[:order][:address_number]  == "2" #登録している配送先を選択した場合
+      @order.address = current_customer.address
+      @order.name = current_customer.full_name
+    elsif params[:order][:address_number] == "2"
       if params[:order][:customer_id] == ""
         redirect_to new_order_path
       else
-        send = Address.find(params[:order][:customer_id])
-        @order.post_code = send.post_code
-        @order.address = send.address
-        @order.name = send.attention_name
+        ship = Address.find(params[:order][:customer_id])
+        @order.post_code = ship.post_code
+        @order.address = ship.address
+        @order.name = ship.attention_name
       end
-    elsif params[:order][:address_number] == "3" #新しく配送先を登録する場合
+    elsif params[:order][:address_number] == "3"
       @order.post_code = params[:order][:post_code]
       @order.address = params[:order][:address]
       @order.name = params[:order][:name]
-      if params[:order][:post_code] == "" || params[:order][:address] == "" || params[:order][:name] == "" #フォームに入力されなかった場合
+      if params[:order][:post_code] == "" || params[:order][:address] == "" || params[:order][:name] == ""
         redirect_to new_order_path
       end
     else
       redirect_to new_order_path
     end
     @cart_items = current_customer.cart_items.all
-    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal}
-    @order.total_payment = @cart_items.inject(800) { |sum, item| sum + item.subtotal}
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @order.total_payment = @cart_items.inject(800) { |sum, item| sum + item.subtotal }
     @shipping_cost = 800
   end
 
@@ -66,7 +67,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :post_code, :address, :name, :total_payment, :shipping_cost)
+    params.require(:order).permit(:payment_method, :post_code, :address, :name, :total_payment, :shipping_cost, :status)
   end
 
   def address_params
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details,dependent: :destroy
-  has_many :items, through: :order_details
-  
+
   def full_name
     customer.last_name + " " + customer.first_name
   end
-  
+
   enum payment_method: {credit_card: 0, transfer: 1 }
   enum status: {payment_wait: 0,payment_confirm: 1,making: 2,shipping_preparation: 3,shipped: 4}
 end
