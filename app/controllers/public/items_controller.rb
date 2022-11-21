ab98036a82abb4864b397546f0194d8f345c2f85
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-11-21 04:19:33 +0000

    Add

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,11 +2,14 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.page(params[:page])
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
+
 
 end
