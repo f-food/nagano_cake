fb543340d18c0985888dc43072aa3d26a299abc9
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-11-21 04:50:59 +0000

    Add

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -11,5 +11,11 @@ class Public::ItemsController < ApplicationController
     @genres = Genre.all
   end
 
+  def search
+    @items = Item.page(params[:page]).per(10).reverse_order
+    @genre = Genre.find(params[:id])
+    @genres = Genre.all
+    @genre_items = @genre.items.page(params[:page]).per(8)
+  end
 
 end
