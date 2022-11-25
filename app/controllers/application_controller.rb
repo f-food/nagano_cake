cee5fd806b04d7616eae4142f12d303c73e47085
EC2 Default User <ec2-user@ip-172-31-22-83.ap-northeast-1.compute.internal>
2022-11-25 03:27:02 +0000

    [Finish]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_customer!, unless: :admin_url
   before_action :authenticate_admin!, if: :admin_url
 
   protected
