df3f2ce713cb296cd86f0433ce1516cbd74e5ca3
EC2 Default User <ec2-user@ip-172-31-22-83.ap-northeast-1.compute.internal>
2022-11-25 04:52:48 +0000

    修正４

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,7 +4,7 @@ class CartItem < ApplicationRecord
   belongs_to :customer
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
-  validates :customer_id, :item_id, :quantity, presence: 
+  validates :customer_id, :item_id, :quantity, presence: true
 
   # 小計を計算
   def subtotal
